8c8da2d09c8b37813ef90767fce6a579071e1fac
vivekvvarshney <4146952+vivekvvarshney@users.noreply.github.com>
2020-02-06 00:28:09 +0530

    commit

---
+++ Assets/Scripts/Pickup_script.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup_script : MonoBehaviour
+{
+    public GameObject thing;
+    public GameObject temp;
+    public Transform guide;
+    public bool isMoving, isInPlace;
+    public float snappingDistance = 1f;
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public GameObject dialogueText;
+    int isSnapped=0;
+=======
+=======
+>>>>>>> parent of bd75c1f... GGJ end
+=======
+>>>>>>> parent of bd75c1f... GGJ end
+=======
+>>>>>>> parent of bd75c1f... GGJ end
+=======
+>>>>>>> parent of bd75c1f... GGJ end
+
+>>>>>>> parent of bd75c1f... GGJ end
+    public Transform Dest;
+
+    public void Start()
+    {
+        //thing.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    void OnMouseDown()
+    {
+        thing.GetComponent<Rigidbody>().useGravity = false;
+        thing.GetComponent<Rigidbody>().isKinematic = true;
+        thing.transform.rotation = guide.transform.rotation;
+        isMoving = true;
+        //thing.transform.position = Vector3.MoveTowards(transform.position, guide.position, 10 * Time.deltaTime);
+        thing.transform.parent = temp.transform.parent;
+        gameObject.GetComponent<NoGravity>().enabled = false;
+    }
+
+    private void OnMouseUp()
+    {
+        //thing.GetComponent<Rigidbody>().useGravity = true;
+        thing.GetComponent<Rigidbody>().isKinematic = false;
+        thing.transform.parent = null;
+        isMoving = false;
+        thing.transform.parent = transform.parent;
+        gameObject.GetComponent<NoGravity>().enabled = true;
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        this.gameObject.GetComponent<AudioSource>().Pause();
+        dialogueText.SetActive(false);
+        if(thing.transform.position.Equals(this.transform.position))
+        {
+            if (isSnapped == 0)
+                isSnapped++;
+
+            if (isSnapped == 1)
+            {
+                guide.GetComponent<PlayMusicWhenPoint>().allsnapped++;
+                isSnapped++;
+            }
+        }
+=======
+>>>>>>> parent of bd75c1f... GGJ end
+=======
+>>>>>>> parent of bd75c1f... GGJ end
+=======
+>>>>>>> parent of bd75c1f... GGJ end
+=======
+>>>>>>> parent of bd75c1f... GGJ end
+=======
+>>>>>>> parent of bd75c1f... GGJ end
+    }
+
+    void Snap()
+    {
+        float dist = Vector3.Distance(transform.position, Dest.position);
+
+        //if(dist<= snappingDistance && !isMoving)
+
+        if(dist<= snappingDistance && !isMoving)
+        {
+            gameObject.GetComponent<NoGravity>().enabled = false;
+            transform.position = Dest.position;
+            transform.rotation = Dest.rotation;
+            thing.GetComponent<Rigidbody>().useGravity = true;
+        }
+
+        
+
+    }
+
+    void Update()
+    {
+        if (isMoving)
+        {
+            thing.transform.position = Vector3.MoveTowards(transform.position, guide.position, 5 * Time.deltaTime);
+        }
+
+        Snap();
+    }
+}
---
+++ Assets/Scripts/PlayMusicWhenPoint.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusicWhenPoint : MonoBehaviour
+{
+    public GameObject video;
+    public GameObject Door;
+    public int allsnapped = 0;
+    //    private RaycastHit hit;
+
+    //    bool playAudio1, canPlay;
+
+    //    [SerializeField]
+    //    private AudioSource source;
+
+    //    [SerializeField]
+    //    private AudioClip clip1;
+    //    Start is called before the first frame update
+    //    void Start()
+    //    {
+    //        source.clip = clip1;
+    //        playAudio1 = false;
+    //        canPlay = true;
+
+    //    }
+
+    //    Update is called once per frame
+    //    void Update()
+    //    {
+    //        if (Physics.Raycast(transform.position, transform.forward, out hit, 100f))
+    //        {
+    //            if (hit.collider.gameObject.name == "Weapon" && canPlay)
+    //            {
+    //                Debug.Log("h");
+    //                playAudio1 = true;
+
+    //                if (playAudio1)
+    //                {
+    //                    hit.collider.GetComponent<AudioSource>().Play();
+    //                }
+
+    //                canPlay = false;
+    //            }
+
+    //        }
+    //    }
+    private void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.Equals(Door) && allsnapped==7)
+        {
+            video.SetActive(true);
+            this.GetComponent<AudioSource>().Stop();
+            if(!video.GetComponent<UnityEngine.Video.VideoPlayer>().isPlaying)
+            {
+                Debug.Log("parent.parent" + this.transform.parent.parent.gameObject.name);
+               //this.transform.parent.parent.gameObject.GetComponent<FirstPersonController>().pause=true;
+            }
+        }
+    }
+}
